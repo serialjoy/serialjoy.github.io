b729703d3626d87c862494684e619317bd44b863
cristian.olarasu@gmail.com <cristian.olarasu@gmail.com>
2015-10-26 05:38:14 +0100

    Header finished

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,3 +3,19 @@
     $('.bar').toggleClass('animate');
   })
 })();
+
+(function ($) {
+  var $mobileNavigationToggleBtn = $('.mobile-navigation-toggle');
+
+    function onBtnClick (e) {
+      var $this = $(this),
+          $selectors = $('.mobile-navigation');
+      $this.toggleClass('is-open');    
+      $selectors.toggleClass('is-open');
+    }
+    
+    $(document).ready(function (){
+      $mobileNavigationToggleBtn.on('click', onBtnClick);
+  });
+    
+})(jQuery);
